20b9663c6c4c0204bff9d3b2b92a7ae9c974b66e
zvicraft <zvicraft@gmail.com>
2024-09-29 13:53:53 +0300

    t

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+        // Other configurations...
+        experimental: {
+          runtime: 'nodejs' // Make sure your API routes use the Node.js runtime
+        },
+      
 };
 if (process.env.NODE_ENV === 'development') {
     await setupDevPlatform();
