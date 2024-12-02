208fbcf7c6c07489ba42ccb74e7946937fe15c16
zviel <zvielkoren@gmail.com>
2024-12-02 21:06:18 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', '@prisma/adapter-d1'],
-  },
+
   env: {
     EDGE_RUNTIME: '1',
   }
