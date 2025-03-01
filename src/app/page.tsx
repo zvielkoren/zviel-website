342d87438cc0cde85330907041cc1401d237c45e
zviel <zvielkoren@gmail.com>
2025-03-01 23:35:10 +0200

    refactor: improve code formatting and readability in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ backgroundColor: '#17212b', color: 'white' }}>
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24"
+      style={{ backgroundColor: "#17212b", color: "white" }}
+    >
       <header className="w-full text-center">
         <h1 className="text-4xl font-bold mb-4">Zviel Koren</h1>
         <p className="text-xl">Full Stack Developer</p>
@@ -22,39 +24,72 @@ export default function Home() {
   
         /> */}
         <p className="text-center max-w-2xl mb-8">
-          Hi, I'm Zviel Koren, a passionate full stack developer with expertise in modern web technologies.
-          I love creating efficient, scalable, and user-friendly applications.
+          Hi, I'm Zviel Koren, a passionate full stack developer with expertise
+          in modern web technologies. I love creating efficient, scalable, and
+          user-friendly applications.
         </p>
         <div className="flex space-x-4">
-                  <Link href="/projects">
-                    <Button color="primary" variant="solid" size="lg" className="font-bold bg-[#2c3e50] hover:bg-[#34495e] transition-colors duration-300 px-8 py-4 rounded-full shadow-lg text-lg">
-                      👨‍💻 View Projects
-                    </Button>
-                  </Link>
-                  {/* <Link href="/contact"><Button color="secondary" as="a" variant="solid" size="lg" className="font-bold hover:bg-[#45505b] transition-colors duration-300">Contact Me</Button></Link> */}        </div>
+          <Link href="/projects">
+            <Button
+              color="primary"
+              variant="solid"
+              size="lg"
+              className="font-bold bg-[#2c3e50] hover:bg-[#34495e] transition-colors duration-300 px-8 py-4 rounded-full shadow-lg text-lg"
+            >
+              👨‍💻 View Projects
+            </Button>
+          </Link>
+        </div>
       </section>
 
       <section id="skills" className="w-full max-w-4xl my-16">
         <h2 className="text-3xl font-bold mb-8 text-center">Skills</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           <div className="bg-gray-800 rounded-lg p-6 shadow-xl">
-            <h3 className="text-2xl font-semibold mb-6 text-center text-white-400">Top Skills</h3>
+            <h3 className="text-2xl font-semibold mb-6 text-center text-white-400">
+              Top Skills
+            </h3>
             <ul className="flex flex-wrap justify-center gap-4">
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">TypeScript</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">Java</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">Python</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">SQL</li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                TypeScript
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Java
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Python
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                SQL
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Rust
+              </li>
             </ul>
           </div>
           <div className="bg-gray-800 rounded-lg p-6 shadow-xl">
-            <h3 className="text-2xl font-semibold mb-6 text-center text-white-400">Other Skills</h3>
+            <h3 className="text-2xl font-semibold mb-6 text-center text-white-400">
+              Other Skills
+            </h3>
             <ul className="flex flex-wrap justify-center gap-4">
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">JavaScript</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">Next.js</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">C#</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">Node.js</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">Kotlin</li>
-              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">etc</li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                JavaScript
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Next.js
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                C#
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Node.js
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                Kotlin
+              </li>
+              <li className="bg-gray-600 rounded-full px-5 py-2 text-lg font-medium shadow-lg transition-all hover:bg-gray-500 hover:scale-105">
+                etc
+              </li>
             </ul>
           </div>
         </div>
@@ -63,12 +98,26 @@ export default function Home() {
       <section id="contact" className="w-full max-w-4xl text-center">
         <h2 className="text-2xl font-bold mb-4"></h2>
         <h2 className="text-2xl font-bold mb-4">Get in Touch</h2>
-        <p className="mb-4">I'm always open to new opportunities and collaborations.</p>
+        <p className="mb-4">
+          I'm always open to new opportunities and collaborations.
+        </p>
 
-        <Button color="primary" variant="solid" size="lg" className="font-bold bg-[#2c3e50] hover:bg-[#34495e] transition-colors duration-300 px-8 py-4 rounded-full shadow-lg text-lg" as="a" href="/contact"> ✉️ Email Me</Button>
+        <Button
+          color="primary"
+          variant="solid"
+          size="lg"
+          className="font-bold bg-[#2c3e50] hover:bg-[#34495e] transition-colors duration-300 px-8 py-4 rounded-full shadow-lg text-lg"
+          as="a"
+          href="/contact"
+        >
+          {" "}
+          ✉️ Email Me
+        </Button>
       </section>
       <footer className="mt-16 text-center">
-        <p>© 2021 - {new Date().getFullYear()} Zviel Koren. All rights reserved.</p>
+        <p>
+          © 2021 - {new Date().getFullYear()} Zviel Koren. All rights reserved.
+        </p>
       </footer>
     </main>
   );
