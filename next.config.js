9390069963c0a80e775921dfaff286fc126f7b18
Zviel <zvielkoren@gmail.com>
2025-07-21 12:30:59 +0300

    refactor: add async rewrites configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   runtime: 'node',
   env: {
     EDGE_RUNTIME: '1',
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/AutoGalleryTool/:path*',
+        destination: 'https://zvielkoren.com/AutoGalleryTool/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
