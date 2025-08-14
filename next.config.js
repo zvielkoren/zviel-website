8d06d2c13d6397e222b7fe963a9a6d2ac20db6d7
Zviel <zvielkoren@gmail.com>
2025-08-14 17:54:51 +0300

    remove AutoGalleryTool paths

--- next.config.js
+++ next.config.js
@@ -5,14 +5,7 @@ const nextConfig = {
   env: {
     EDGE_RUNTIME: '1',
   },
-  async rewrites() {
-    return [
-      {
-        source: '/AutoGalleryTool/:path*',
-        destination: 'https://zvielkoren.com/AutoGalleryTool/:path*',
-      },
-    ];
-  },
+
 }
 
 module.exports = nextConfig
